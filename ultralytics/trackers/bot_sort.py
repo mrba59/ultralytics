96016ff974873dd8976c3280bae997931b145712
mrba59 <rbenallal59@gmail.com>
2024-08-19 17:47:03 +0200

    remove the divide by 2

--- ultralytics/trackers/bot_sort.py
+++ ultralytics/trackers/bot_sort.py
@@ -226,7 +226,7 @@ class BOTSORT(BYTETracker):
             dists = matching.fuse_score(dists, detections)
 
         if self.args.with_reid and self.encoder is not None:
-            emb_dists = matching.embedding_distance(tracks, detections) / 2.0
+            emb_dists = matching.embedding_distance(tracks, detections)
             emb_dists[emb_dists > self.appearance_thresh] = 1.0
             emb_dists[dists_mask] = 1.0
             dists = np.minimum(dists, emb_dists)
