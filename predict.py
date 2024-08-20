258720ba37bc2dc5e4db48a23e017312896c3893
mrba59 <rbenallal59@gmail.com>
2024-08-20 16:29:55 +0200

    reformat predict and track , so that it read parameters from yaml file, and store the labels inside origin repo

--- predict.py
+++ predict.py
@@ -1,76 +1,56 @@
-import argparse
-from argparse import ArgumentParser
+import os
+
+import yaml
 from ultralytics import YOLO
 import time
 import tqdm
 import numpy as np
 
 
-def main(args):
-    print(f"Model Path: {args.model_path}")
-    print(f"Video Path: {args.video_path}")
-    print(f"IOU Threshold: {args.iou}")
-    print(f"Confidence Threshold: {args.conf}")
-    print(f"Output Path: {args.output_path}")
-    print(f"Output Name: {args.output_name}")
-    print(f"Stream: {args.stream}")
-    print(f"Save TXT: {args.save_txt}")
-    print(f"Save Confidence: {args.save_conf}")
-    print(f"vid_stride: {args.vid_stride}")
+def main(config):
+    print(f"Model Path: {config['model_path']}")
+    print(f"Video Path: {config['video_path']}")
+    print(f"IOU Threshold: {config['iou']}")
+    print(f"Confidence Threshold: {config['conf']}")
+    print(f"Output Path: {config['output_path']}")
+    print(f"Output Name: {config['output_name']}")
+    print(f"Stream: {config['stream']}")
+    print(f"Save TXT: {config['save_txt']}")
+    print(f"Save Confidence: {config['save_conf']}")
+    print(f"vid_stride: {config['vid_stride']}")
 
     nb_pred_list = []
-    model = YOLO(args.model_path)
+    model = YOLO(config['model_path'])
     model.to('cuda')
     start = time.time()
     for result in tqdm.tqdm(
-            model.predict(args.video_path, conf=args.conf, iou=args.iou, save=True, stream=args.stream, save_crop=args.save_crop, save_txt=args.save_txt,
+            model.predict(config['video_path'],
+                          conf=config['conf'],
+                          iou=config['iou'],
+                          save=config['save'],
+                          stream=config['stream'],
+                          save_crop=config['save_crop'],
+                          save_txt=config['save_txt'],
                           batch=8,
-                          save_conf=args.save_conf, project=args.output_path, name=args.output_name, vid_stride=1)):
+                          save_conf=config['save_conf'],
+                          project=config['output_path'],
+                          name=config['output_name'],
+                          vid_stride=config['vid_stride'])):
         nb_pred_list.append(len(result.boxes))
         pass
     print(f"elapsed time  {time.time() - start}")
     print(f" mean nb of object inside image: {np.mean(np.array(nb_pred_list))}")
 
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process video with YOLOv8")
+    # Load the YAML configuration
+    with open('predict.yaml', 'r') as file:
+        config = yaml.safe_load(file)
 
-    parser.add_argument("model_path", type=str, help="Path to the model file (e.g., best.pt)")
-    parser.add_argument("video_path", type=str, help="Path to the video file (e.g., DJI_0070_cut.MP4)")
+    # Run the main function with the loaded config
+    main(config)
+    config_save_path = os.path.join(config['output_path'], ['output_name'], 'predict.yaml')
+    with open(config_save_path, 'w') as file:
+        yaml.dump(config, file)
 
-    parser.add_argument("--iou", type=float, default=0.5, help="IOU threshold for NMS (default: 0.5)")
-    parser.add_argument("--conf", type=float, default=0.25, help="Confidence threshold for detection (default: 0.25)")
-
-    parser.add_argument("--output_path", type=str, default="./",
-                        help="Path to save the output files (default: current directory)")
-    parser.add_argument("--output_name", type=str, default="output", help="Name for the output files (default: output)")
-
-    parser.add_argument("--stream", action="store_true", help="Stream the video while processing")
-    parser.add_argument("--save_txt", action="store_true", help="Save the detection results to a TXT file")
-    parser.add_argument("--save_conf", action="store_true", help="Save the confidence scores in the output")
-    parser.add_argument("--vid_stride", type=int, help="Take every x frames from a video")
-    parser.add_argument("--save_crop", action="store_true", help="Save as crop object detected")
-
-    args = parser.parse_args()
-    main(args)
-
-"""results = model(args.video_path, conf=args.conf, save=True, stream=args.stream, save_txt=args.save_txt,
-                   save_conf=args.save_conf, show_labels=True, show_boxes=True, show_conf=True,
-                   vid_stride=args.vid_stride, iou=args.iou,
-                   project=args.output_path, name=args.output_name)"""
-
-# model = YOLO("/home/reda/Documents/ultralytics/runs/segment/yolov8x_seg/weights/best.pt")
-# model = YOLO("/home/reda/Documents/ultralytics/runs/detect/yolov8n_custom/weights/best.pt")
-# model = YOLO("/home/reda/Documents/ultralytics/runs/detect/yolov8_robo_human/weights/best.pt")
-# model = YOLO("rosen.pt")
-# model = YOLO("/home/reda/Documents/ultralytics/runs/detect/yolov8_robo2/weights/best.pt")
-
-# from ndarray
-# im2 = cv2.imread("/home/reda/Documents/Tracking/results/images_det/frame_6000.jpg")
-# video = "/home/reda/Documents/Dataset/france_croatie_fps10.mp4"
-# video = "/home/reda/Documents/drone/usld_lefrin_27_sept/DJI_0039.MP4"
-# video = "/home/reda/Documents/drone/usld_17_06_sept/DJI_0010.MP4"
-# video = "/home/reda/Documents/drone/usld_17_06_sept/DJI_0008.MP4"
-# dir_ = "/home/reda/Documents/Dataset/france_croatie_cut"
-
+    print(f"Configuration file saved to: {config_save_path}")
--- track.py
+++ track.py
@@ -1,30 +1,68 @@
+import os
+
+import yaml
 from ultralytics import YOLO
 import time
 import tqdm
+import numpy as np
 
 
-# model = YOLO("/home/reda/Documents/projets/ultralytics/runs/detect/yolov8_robo_human_aerial_view_person/weights/best.pt")
-# model = YOLO("/home/reda/Documents/projets/ultralytics/runs/detect/yolov8_aerial_view_person_1280p2/weights/best.pt")
-#model = YOLO("yolov8x.pt")
-# model = YOLO("/home/reda/Documents/projets/ultralytics/runs/detect/yolov8_robo2/weights/best.pt")
+def main(config):
+    print(f"Model Path: {config['model_path']}")
+    print(f"Video Path: {config['video_path']}")
+    print(f"IOU Threshold: {config['iou']}")
+    print(f"Confidence Threshold: {config['conf']}")
+    print(f"Output Path: {config['output_path']}")
+    print(f"Output Name: {config['output_name']}")
+    print(f"Stream: {config['stream']}")
+    print(f"Save TXT: {config['save_txt']}")
+    print(f"Save Confidence: {config['save_conf']}")
+    print(f"vid_stride: {config['vid_stride']}")
+    print(f"tracker: {config['tracker']}")
 
-# video = "/home/reda/Documents/Dataset/france_croatie_fps10.mp4"
-# video = "/home/reda/Documents/drone/tournoi_warhem/DJI_0070_cut.MP4"
-# video = "/home/reda/Documents/drone/usld_17_06_sept/DJI_0010.MP4"
-# video = "/home/reda/Documents/drone/usld_17_06_sept/DJI_0008.MP4"
+    nb_pred_list = []
+    model = YOLO(config['model_path'])
+    model.to('cuda')
+    start = time.time()
+    for result in tqdm.tqdm(
+            model.track(config['video_path'],
+                        conf=config['conf'],
+                        iou=config['iou'],
+                        save=config['save'],
+                        stream=config['stream'],
+                        save_crop=config['save_crop'],
+                        save_txt=config['save_txt'],
+                        batch=8,
+                        save_conf=config['save_conf'],
+                        project=config['output_path'],
+                        name=config['output_name'],
+                        vid_stride=config['vid_stride'],
+                        tracker=config['tracker'])):
+        nb_pred_list.append(len(result.boxes))
+        pass
+    print(f"elapsed time  {time.time() - start}")
+    print(f" mean nb of object inside image: {np.mean(np.array(nb_pred_list))}")
 
 
-model = YOLO("/home/reda/Documents/projets/ultralytics/runs/detect/yolov8_robo/weights/best.pt")
-model.to('cuda')
-video = "/home/reda/Documents/tetegehm_malo.mp4"
+if __name__ == "__main__":
+    # Load the YAML configuration
+    with open('predict.yaml', 'r') as file:
+        config = yaml.safe_load(file)
 
+    # Run the main function with the loaded config
+    main(config)
+    config_save_path = os.path.join(config['output_path'], ['output_name'], 'track.yaml')
+    with open(config_save_path, 'w') as file:
+        yaml.dump(config, file)
 
+    print(f"Configuration file saved to: {config_save_path}")
 
-nb_pred_list = []
+"""nb_pred_list = []
 start = time.time()
 for result in tqdm.tqdm(
         model.track(video, conf=0.4, persist=True, tracker="botsort.yaml", save=True, stream=True, save_txt=True,
-                    batch=16, save_conf=True, show_labels=True, boxes=True, show_conf=True, vid_stride=6, iou=0.7,
+                    batch=16, save_conf=True, show_labels=True, boxes=True, show_conf=True, vid_stride=2, iou=0.7,
                     project="/home/reda/Documents/projets/results_yolo/", name="tournoi_yolov8x")):
-    nb_pred_list.append(len(result.boxes))
+    nb_pred_list.append(len(result.boxes))"""
+
 
